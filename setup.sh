210f725b55e5183e10d70963892d5877bb22fcdb
Murat Gözel <murat@gozel.com.tr>
2025-05-10 23:20:20 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+apt install -y pgbackrest
+
+mkdir -p -m 770 /var/log/pgbackrest
+chown postgres:postgres /var/log/pgbackrest
+mkdir -p /etc/pgbackrest
+mkdir -p /etc/pgbackrest/conf.d
+touch /etc/pgbackrest/pgbackrest.conf
+chmod 640 /etc/pgbackrest/pgbackrest.conf
+chown postgres:postgres /etc/pgbackrest/pgbackrest.conf
+sudo -u postgres pgbackrest
+
+pgconf=/etc/postgresql/17/main/postgresql.conf
+echo "archive_mode = on" >> $pgconf
+echo "archive_command = 'pgbackrest --stanza=app archive-push %p'" >> $pgconf
+
+echo "pgbackrest installed successfully."
