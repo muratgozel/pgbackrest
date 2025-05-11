26a4a7c2f879839778311c4a910118c351be1a70
Murat Gözel <murat@gozel.com.tr>
2025-05-11 15:18:49 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,4 +17,14 @@ pgconf=/etc/postgresql/17/main/postgresql.conf
 echo "archive_mode = on" >> $pgconf
 echo "archive_command = 'pgbackrest --stanza=app archive-push %p'" >> $pgconf
 
+sudo -u postgres pgbackrest --stanza=app --pg1-port=5432 --log-level-console=info stanza-create
+service postgresql restart
+sudo -u postgres pgbackrest --stanza=app --pg1-port=5432 --log-level-console=info check
+pgbackrest_check_result=$?
+
+if [ $pgbackrest_check_result -ne 0 ]; then
+  echo "pgbackrest check failed."
+  exit $pgbackrest_check_result
+fi
+
 echo "pgbackrest installed successfully."
